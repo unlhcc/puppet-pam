944baef779a60ee755b9e1a993688bf5ad8cdaa4
Garhan Attebury <garhan.attebury@unl.edu>
2014-12-09 17:10:58 -0600

    initial stuff with default templates

---
+++ manifests/init.pp
@@ -0,0 +1,57 @@
+#
+# pam
+#
+
+class pam (
+    $enable_duo                         = $pam::params::enable_duo,
+    $enable_sssd                        = $pam::params::enable_sssd,
+    $pam_d_sshd_file                    = $pam::params::pam_d_sshd_file,
+    $pam_d_sshd_template                = $pam::params::pam_d_sshd_template,
+    $pam_d_system_auth_ac_file          = $pam::params::pam_d_system_auth_ac_file,
+    $pam_d_system_auth_ac_template      = $pam::params::pam_d_system_auth_ac_template,
+    $pam_d_password_auth_ac_file        = $pam::params::pam_d_password_auth_ac_file,
+    $pam_d_password_auth_ac_template    = $pam::params::pam_d_password_auth_ac_template,
+) inherits pam::params {
+
+
+    validate_bool($enable_duo)
+    validate_bool($enable_sssd)
+    validate_absolute_path($pam_d_sshd_file)
+    validate_absolute_path($pam_d_system_auth_ac_file)
+    validate_absolute_path($pam_d_password_auth_ac_file)
+    validate_string($pam_d_sshd_template)
+    validate_string($pam_d_system_auth_ac_template)
+    validate_string($pam_d_password_auth_ac_template)
+
+    
+    file { 'pam_d_sshd':
+        ensure  => file,
+        path    => $pam_d_sshd_file,
+        owner   => 'root',
+        group   => 'root',
+        mode    => '0644',
+        content => template($pam_d_sshd_template),
+    }
+
+    file { 'pam_system_auth_ac':
+        ensure  => file,
+        path    => $pam_d_system_auth_ac_file,
+        owner   => 'root',
+        group   => 'root',
+        mode    => '0644',
+        content => template($pam_d_system_auth_ac_template),
+    }
+
+    if $::operatingsystemmajrelease >= '6' {
+        file { 'pam_password_auth_ac':
+            ensure  => file,
+            path    => $pam_d_password_auth_ac_file,
+            owner   => 'root',
+            group   => 'root',
+            mode    => '0644',
+            content => template($pam_d_password_auth_ac_template),
+        }
+    }
+
+}
+
---
+++ manifests/params.pp
@@ -0,0 +1,42 @@
+#
+# pam params
+#
+
+class pam::params {
+
+    $enable_duo = false
+    $enable_sssd = false
+
+    $pam_d_sshd_file = '/etc/pam.d/sshd'
+    $pam_d_system_auth_ac_file = '/etc/pam.d/system-auth-ac'
+    $pam_d_password_auth_ac_file = '/etc/pam.d/password-auth-ac'
+
+
+    case $::osfamily {
+        'RedHat' {
+            case $::operatingsystemmajrelease {
+                '5': {
+                    $pam_d_sshd_template = 'pam/sshd.el5.erb'
+                    $pam_d_system_auth_ac_template = 'pam/system-auth-ac.el5.erb'
+                }
+                '6': {
+                    $pam_d_sshd_template = 'pam/sshd.el6.erb'
+                    $pam_d_password_auth_ac_template = 'pam/password-auth-ac.el6.erb'
+                    $pam_d_system_auth_ac_template = 'pam/system-auth-ac.el6.erb'
+                }
+                '7': {
+                    $pam_d_sshd_template = 'pam/sshd.el7.erb'
+                    $pam_d_password_auth_ac_template = 'pam/password-auth-ac.el7.erb'
+                    $pam_d_system_auth_ac_template = 'pam/system-auth-ac.el7.erb'
+                }
+                default: {
+                    fail('This PAM module is only supported on EL 5, 6, and 7.')
+                }
+            }
+        }
+        default: {
+            fail('This PAM module is only supported on RedHat and derivatives.')
+        }
+    }
+
+}
