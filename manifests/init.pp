0b0633d407ddc489a3566c50062aedc02d5a5366
John Thiltges <jthiltges2@unl.edu>
2020-10-23 12:16:04 -0500

    Remove the *-ac (authconfig) suffix from the managed PAM files

--- manifests/init.pp
+++ manifests/init.pp
@@ -7,10 +7,10 @@ class pam (
     $enable_sssd                        = $pam::params::enable_sssd,
     $pam_d_sshd_file                    = $pam::params::pam_d_sshd_file,
     $pam_d_sshd_template                = $pam::params::pam_d_sshd_template,
-    $pam_d_system_auth_ac_file          = $pam::params::pam_d_system_auth_ac_file,
-    $pam_d_system_auth_ac_template      = $pam::params::pam_d_system_auth_ac_template,
-    $pam_d_password_auth_ac_file        = $pam::params::pam_d_password_auth_ac_file,
-    $pam_d_password_auth_ac_template    = $pam::params::pam_d_password_auth_ac_template,
+    $pam_d_system_auth_file             = $pam::params::pam_d_system_auth_file,
+    $pam_d_system_auth_template         = $pam::params::pam_d_system_auth_template,
+    $pam_d_password_auth_file           = $pam::params::pam_d_password_auth_file,
+    $pam_d_password_auth_template       = $pam::params::pam_d_password_auth_template,
     $session_include                    = $pam::params::session_include,
 ) inherits pam::params {
 
@@ -18,11 +18,11 @@ class pam (
     validate_bool($enable_duo)
     validate_bool($enable_sssd)
     validate_absolute_path($pam_d_sshd_file)
-    validate_absolute_path($pam_d_system_auth_ac_file)
-    validate_absolute_path($pam_d_password_auth_ac_file)
+    validate_absolute_path($pam_d_system_auth_file)
+    validate_absolute_path($pam_d_password_auth_file)
     validate_string($pam_d_sshd_template)
-    validate_string($pam_d_system_auth_ac_template)
-    validate_string($pam_d_password_auth_ac_template)
+    validate_string($pam_d_system_auth_template)
+    validate_string($pam_d_password_auth_template)
 
 
     file { 'pam_d_sshd':
@@ -34,23 +34,23 @@ class pam (
         content => template($pam_d_sshd_template),
     }
 
-    file { 'pam_system_auth_ac':
+    file { 'pam_system_auth':
         ensure  => file,
-        path    => $pam_d_system_auth_ac_file,
+        path    => $pam_d_system_auth_file,
         owner   => 'root',
         group   => 'root',
         mode    => '0644',
-        content => template($pam_d_system_auth_ac_template),
+        content => template($pam_d_system_auth_template),
     }
 
     if $::operatingsystemmajrelease >= '6' {
-        file { 'pam_password_auth_ac':
+        file { 'pam_password_auth':
             ensure  => file,
-            path    => $pam_d_password_auth_ac_file,
+            path    => $pam_d_password_auth_file,
             owner   => 'root',
             group   => 'root',
             mode    => '0644',
-            content => template($pam_d_password_auth_ac_template),
+            content => template($pam_d_password_auth_template),
         }
     }
 
--- manifests/params.pp
+++ manifests/params.pp
@@ -9,8 +9,8 @@ class pam::params {
     $session_include = undef
 
     $pam_d_sshd_file = '/etc/pam.d/sshd'
-    $pam_d_system_auth_ac_file = '/etc/pam.d/system-auth-ac'
-    $pam_d_password_auth_ac_file = '/etc/pam.d/password-auth-ac'
+    $pam_d_system_auth_file = '/etc/pam.d/system-auth'
+    $pam_d_password_auth_file = '/etc/pam.d/password-auth'
 
 
     case $::osfamily {
@@ -18,22 +18,22 @@ class pam::params {
             case $::operatingsystemmajrelease {
                 '5': {
                     $pam_d_sshd_template = 'pam/sshd.el5.erb'
-                    $pam_d_system_auth_ac_template = 'pam/system-auth-ac.el5.erb'
+                    $pam_d_system_auth_template = 'pam/system-auth.el5.erb'
                 }
                 '6': {
                     $pam_d_sshd_template = 'pam/sshd.el6.erb'
-                    $pam_d_password_auth_ac_template = 'pam/password-auth-ac.el6.erb'
-                    $pam_d_system_auth_ac_template = 'pam/system-auth-ac.el6.erb'
+                    $pam_d_password_auth_template = 'pam/password-auth.el6.erb'
+                    $pam_d_system_auth_template = 'pam/system-auth.el6.erb'
                 }
                 '7': {
                     $pam_d_sshd_template = 'pam/sshd.el7.erb'
-                    $pam_d_password_auth_ac_template = 'pam/password-auth-ac.el7.erb'
-                    $pam_d_system_auth_ac_template = 'pam/system-auth-ac.el7.erb'
+                    $pam_d_password_auth_template = 'pam/password-auth.el7.erb'
+                    $pam_d_system_auth_template = 'pam/system-auth.el7.erb'
                 }
                 '8': {
                     $pam_d_sshd_template = 'pam/sshd.el8.erb'
-                    $pam_d_password_auth_ac_template = 'pam/password-auth-ac.el8.erb'
-                    $pam_d_system_auth_ac_template = 'pam/system-auth-ac.el8.erb'
+                    $pam_d_password_auth_template = 'pam/password-auth.el8.erb'
+                    $pam_d_system_auth_template = 'pam/system-auth.el8.erb'
                 }
                 default: {
                     fail('This PAM module is only supported on EL 5, 6, 7, and 8.')
